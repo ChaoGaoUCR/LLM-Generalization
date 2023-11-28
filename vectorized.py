6642efa07aa760622a429592ca16fd00ed3de000
Chao Gao <86901408+ChaoGaoUCR@users.noreply.github.com>
2023-11-27 21:26:09 -0800

    Add files via upload

---
+++ vectorized.py
@@ -0,0 +1,198 @@
+import itertools
+from math import factorial
+
+import torch
+from torch.utils.data import Dataset
+import random
+
+
+# calculate vector size in MB
+def _get_tensor_size_mb(a: torch.Tensor):
+    return (a.element_size() * a.nelement()) / 2 ** 20
+
+
+# element_size can be an integer or torch.dtype
+def _get_memory_size_mb(element_size, length):
+    if isinstance(element_size, torch.dtype):
+        element_size = torch.tensor(0, dtype=element_size).element_size()
+    return (element_size * length) / 2 ** 20
+
+
+def mod_sum(x: torch.Tensor):
+    return torch.fmod(torch.sum(x), 10)
+
+
+def maj_vote(x: torch.Tensor):
+    return torch.atleast_1d(torch.mode(x).values)
+
+def multi_step(x:torch.Tensor):
+    pointer_idx = 0
+    step = 0
+    while(x[pointer_idx] > x[x[pointer_idx]]):
+        step = step + 1
+        # print(f"pointer_idx: {pointer_idx}, step {step}")
+        pointer_idx = x[pointer_idx]
+
+    return x[pointer_idx], step
+
+class VectorPVR(Dataset):
+    sample_size = 11  # pointer + 10 digits
+    dtype = torch.long
+
+    def __init__(self,
+                 name: str,
+                 size: int,
+                 complexity: int,  # size of the window (m in the article)
+                 holdout: int,  # number of permutations to holdout ('holdout-i' or '# holdout' in article)
+                 aggregation_method: str = 'mod_sum',
+                 adversarial: bool = False):
+
+        size = int(size)
+        if size > 10 ** 8:
+            raise ValueError('Size is to big! Ain`t nobody got RAM for that!')
+
+        if not isinstance(complexity, int):
+            raise ValueError(f"complexity must be an integer. Got {type(complexity).__name__} instead.")
+
+        if complexity < 0 or complexity > 9:
+            raise ValueError(f'complexity must be in [0,9]. Got complexity={complexity} instead.')
+
+        if not isinstance(holdout, int):
+            raise ValueError(f"holdout must be an integer. Got {type(holdout).__name__} instead.")
+
+        if holdout < 0 or holdout > factorial(complexity + 1):
+            raise ValueError(f"holdout must be in [0, {factorial(complexity + 1)}]. Got holdout={holdout} instead.")
+
+        if complexity == 0 and holdout != 0:
+            raise ValueError('Can`t holdout when complexity is 0.')
+
+        self.name = name
+        self.size = size
+        self.complexity = complexity
+        self.aggregation_method = aggregation_method
+        self.holdout = holdout
+        self.adversarial = adversarial
+        self.aggregator = self._set_up_aggregator(aggregation_method)
+
+        expected_memory_mb = _get_memory_size_mb(self.dtype, self.size * (self.sample_size + 1))
+        print(f"Expected '{name}' size: {expected_memory_mb:.3f}MB")
+
+        # data is built from numbers 0-9
+        self.data = torch.randint(10, size=(size, self.sample_size), dtype=self.dtype)
+
+        # the pointer value should be limited so the window will fit in the samples
+        # self.data[:, 0] = torch.randint(10 - complexity, size=[size], dtype=self.dtype)
+        
+        # if holdout > 0:
+        #     if adversarial:
+        #         self._insert_permutations()
+        #     else:
+        #         self._remove_permutations()
+        # import pdb
+        # pdb.set_trace()
+        # calculate values
+        self.values = torch.zeros(size, dtype=self.dtype)
+        self.steps = torch.zeros(size, dtype=self.dtype)
+        
+        # get random value from 1 - 3, we only use "<" for multi-steps
+        # self.steps = torch.randint(low=1,high =4 ,size = self.size)
+        if aggregation_method != 'multi-step':
+            for idx, sample in enumerate(self.data):
+                # torch.sort(sample)
+                self.values[idx] = self.calc_value(sample)
+        else:
+            for idx, sample in enumerate(self.data):
+                # sample = torch.sort(sample, descending=True)[0]
+                self.values[idx], self.steps[idx] = self.cal_multi_step(sample)
+
+        # move to cuda - might not be helpful?
+        # self.data.to('cuda')
+        # self.values.to('cuda')
+
+    @staticmethod
+    def _set_up_aggregator(aggregation_method):
+        if aggregation_method == 'mod_sum':
+            aggregator = mod_sum
+        elif aggregation_method == 'min':
+            aggregator = torch.min
+        elif aggregation_method == 'max':
+            aggregator = torch.max
+        elif aggregation_method == 'median':
+            aggregator = torch.median
+        elif aggregation_method == 'maj_vote':
+            aggregator = maj_vote
+        elif aggregation_method == 'multi-step':
+            aggregator = multi_step
+        else:
+            raise ValueError('Unknown aggregation method.')
+
+        return aggregator
+
+    def get_holdout_permutations(self):
+        # find the first 'holdout' permutations of (0, 1, ... , complexity)
+        permutations_iterator = itertools.permutations(range(self.complexity + 1))
+        return [next(permutations_iterator) for i in range(self.holdout)]
+
+    def _remove_permutations(self):
+        # create # holdout permutation of (0, 1, ... , complexity)
+        permutations = self.get_holdout_permutations()
+
+        for permutation in permutations:
+            # indicator of where the permutation was found (default true)
+            sample_has_permutation = torch.ones(self.size, dtype=torch.bool)
+
+            # compare the value window to the permutation
+            # if there's a mismatch in a single index mark as False
+            pointers = self.data[:, 0].long()
+            for offset in range(self.complexity + 1):
+                curr_value = self.data[range(self.size), pointers + 1 + offset]
+                sample_has_permutation[curr_value != permutation[offset]] = False
+
+            # add 1 (mod 10) to the vectors where the permutation was found (excluding the pointer at index 0)
+            self.data[sample_has_permutation, 1:] = torch.fmod(self.data[sample_has_permutation, 1:] + 1, 10)
+    # def set_multiple_stesp(self):
+        
+
+    def _insert_permutations(self):
+        # create # holdout permutation of (0, 1, ... , complexity)
+        permutations = torch.tensor(self.get_holdout_permutations(), dtype=self.dtype)
+        permutations_selector = torch.randint(permutations.shape[0], size=[self.size])
+
+        pointers = self.data[:, 0].long()
+        for offset in range(self.complexity + 1):
+            self.data[range(self.size), pointers + 1 + offset] = permutations[permutations_selector, offset]
+
+    def calc_value(self, sample):
+        # if self.aggregation_method
+        pointer_idx = sample[0] + 1
+        return self.aggregator(sample[pointer_idx: pointer_idx + self.complexity + 1])
+
+    def cal_multi_step(self, sample):
+        # pointer_idx = sample[0] + 1
+        return self.aggregator(sample)
+    def __getitem__(self, idx):
+        return self.data[idx], self.values[idx].item()
+
+    def __len__(self):
+        return self.size
+
+    def print(self):
+        for idx, (sample, label) in enumerate(self):
+            print(f'{idx}:\t {sample.tolist()} -> {label.tolist()}')
+
+
+def main():
+    p = 2
+    ds = VectorPVR(size=10**p, complexity=2, holdout=2, aggregation_method='multi-step', adversarial=False, name="test")
+    print(p, ':', _get_tensor_size_mb(ds.data))
+    import pdb
+    pdb.set_trace()
+    for idx in range(len(ds)):
+        if ds.aggregation_method != 'multi-step':
+            print(ds[idx])
+        else:
+            print(f"{ds.data[idx]}  {ds.values[idx]}  {ds.steps[idx]}")
+
+
+if __name__ == '__main__':
+    main()
---
+++ visual_block_style.py
@@ -0,0 +1,177 @@
+import torch
+from torch.utils.data import Dataset
+from torchvision import datasets
+from torchvision import transforms
+
+TRAIN_SET = datasets.MNIST(root='../data', train=True, download=True)
+TEST_SET = datasets.MNIST(root='../data', train=False, download=True)
+
+HOLDOUT_CLASSES = {0: [],  # top_left
+                   1: [1, 2, 3],  # top_right
+                   2: [4, 5, 6],  # bottom_left
+                   3: [7, 8, 9, 0]}  # bottom_right
+
+TRANSLATION_FACTOR = (40 - 28) / 2 / 40
+TRANSFORM = transforms.Compose([
+    transforms.CenterCrop([40, 40]),  # basically pads with zeros
+    transforms.RandomAffine(0, translate=[TRANSLATION_FACTOR, TRANSLATION_FACTOR]),
+    transforms.ToTensor()
+])
+def map_to_coordinates(value):
+    for key, values in HOLDOUT_CLASSES.items():
+        if value in values:
+            return key
+    return None
+
+def _get_value(labels):
+    """
+    get the value based on the pointer
+
+    Args:
+        labels: a tensor of length 4. where labels[0] is the pointer
+
+    Returns:
+        The value
+    """
+
+    pointer = labels[0]
+    if 0 <= pointer <= 3:
+        value = labels[1]
+    elif 4 <= pointer <= 6:
+        value = labels[2]
+    else:
+        value = labels[3]
+
+    return value
+
+
+class BlockStylePVR(Dataset):
+    def __init__(self,
+                 train: bool,
+                 mode: str = "iid",
+                 size: int = None):
+        """
+
+        Args:
+            train: whether to use MNIST train set, else use the test set.
+            mode: "holdout" or "adversarial" or "iid".
+            size: dataset size
+        """
+
+        self.ds = TRAIN_SET if train else TEST_SET
+
+        if size is not None and size > len(self.ds) // 4:
+            raise ValueError(f"Requested dataset size is too big. Can be up too {len(self.ds) // 4}.")
+
+        # use maximum size if size is null
+        self.pvr_ds_size = len(self.ds) // 4 if size is None else int(size)
+        self.steps = torch.zeros(self.pvr_ds_size, dtype=torch.long)
+        self.value = torch.zeros(self.pvr_ds_size, dtype=torch.long)
+        # the labels (0-9) of the images in the dataset
+        ds_labels = []
+        for idx, (_, y) in enumerate(self.ds):
+            ds_labels.append(y)
+        ds_labels = torch.tensor(ds_labels)
+
+        # dtype must be torch.long, otherwise it will crash during training
+        self.idxs = torch.zeros([self.pvr_ds_size, 4], dtype=torch.long)
+        self.labels = torch.zeros([self.pvr_ds_size, 4], dtype=torch.long)  # 0-9
+
+        if mode == 'iid':
+            # the labels (0-9) of the 4 digits in each sample
+            self.labels = ds_labels.reshape([-1, 4])  # group into 4's
+            self.labels = self.labels[:self.pvr_ds_size]  # trim to requested size
+
+            # the original idx of the 4 digits in each sample
+            self.idxs = torch.arange(len(self.ds)).reshape([-1, 4])  # group into 4's
+            self.idxs = self.idxs[:self.pvr_ds_size]  # trim to requested size
+
+        elif mode == "holdout":
+            # sample from the ds excluding the labels that are held out
+            for i, holdout_class in HOLDOUT_CLASSES.items():
+                probs = torch.ones(len(self.ds))
+                for label in holdout_class:
+                    probs[ds_labels == label] = 0
+                curr_idxs = torch.multinomial(probs, self.pvr_ds_size)
+                self.idxs[:, i] = curr_idxs
+                self.labels[:, i] = ds_labels[curr_idxs]
+
+        elif mode == "adversarial":
+            # sample from the ds only where the labels are held out
+            for i, holdout_class in HOLDOUT_CLASSES.items():
+                probs = torch.ones(len(self.ds)) if i == 0 else torch.zeros(len(self.ds))
+                for label in holdout_class:
+                    probs[ds_labels == label] = 1
+                curr_idxs = torch.multinomial(probs, self.pvr_ds_size)
+                self.idxs[:, i] = curr_idxs
+                self.labels[:, i] = ds_labels[curr_idxs]
+        elif mode == "multi-step":
+            print(f"Multi-step size: {self.pvr_ds_size}")
+            for i, holdout_class in HOLDOUT_CLASSES.items():
+                probs = torch.ones(len(self.ds))
+                for label in holdout_class:
+                    probs[ds_labels == label] = 0
+                curr_idxs = torch.multinomial(probs, self.pvr_ds_size)
+                self.idxs[:, i] = curr_idxs
+                self.labels[:, i] = ds_labels[curr_idxs]
+            
+            for idx in range(len(self.labels)):
+                tmp = []
+                for tmp_label in self.labels[idx]:
+                    tmp.append(map_to_coordinates(tmp_label))
+                # print(self.labels[idx])
+                # print(tmp)
+                step = 0
+                pointer = 0 
+                while(self.labels[idx][pointer] > self.labels[idx][map_to_coordinates(self.labels[idx][pointer])]):
+                    if(idx == 0):
+                        print(f"current value {self.labels[idx][pointer]}, current pointer {pointer}")
+                        print(f"next value {self.labels[idx][map_to_coordinates(self.labels[idx][pointer])]}, next pointer {tmp[pointer]}")
+                    step = step + 1
+                    pointer = tmp[pointer]
+                self.steps[idx] = step
+                self.value[idx] = self.labels[idx][pointer]
+        else:
+            raise ValueError("Unknown dataset mode.")
+
+    def __getitem__(self, idx):
+        labels = self.labels[idx]
+        idxs = self.idxs[idx]
+
+        # for each label, get the matching image from the ds using the idx
+        # transform it (fit into 40x40 and translate randomly)
+        # and then put in the appropriate location in the result image
+        x = torch.zeros([1, 80, 80])
+        x[0, :40, :40] = TRANSFORM(self.ds[idxs[0]][0])
+        x[0, :40, 40:] = TRANSFORM(self.ds[idxs[1]][0])
+        x[0, 40:, :40] = TRANSFORM(self.ds[idxs[2]][0])
+        x[0, 40:, 40:] = TRANSFORM(self.ds[idxs[3]][0])
+
+        # calculate the value based on the pointer
+        value = _get_value(labels)
+
+        return x, value
+
+    def __len__(self):
+        return self.pvr_ds_size
+
+
+def main():
+    import matplotlib.pyplot as plt
+
+    ds = BlockStylePVR(train=True, mode='multi-step', size=None)
+    # print(len(ds))
+    # for i in range(50):
+    #     x, y = ds[i]
+    #     plt.imshow(x[0], cmap='gray')
+    #     plt.title(y.item())
+    #     plt.show()
+    
+    plt.imshow(ds[0][0][0], cmap='gray')
+    plt.savefig('test.png')
+    print(ds[0][0].shape)
+    import pdb
+    pdb.set_trace()
+
+if __name__ == "__main__":
+    main()
